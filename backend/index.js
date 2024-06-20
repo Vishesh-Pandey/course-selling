7826a36b89004409e4bcfe88234965482d08e1dc
Vishesh-Pandey <visheshpandey24@gmail.com>
2024-06-20 22:35:08 +0530

    initialized prisma

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World from typescript!");
 });
 app.listen(8000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 8000");
 });
--- backend/index.ts
+++ backend/index.ts
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World from typescript!");
 });
 
 app.listen(8000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 8000");
 });
